31556f06bba6b3f1c55747d23baa0808d5f3a31e
Ghosh <parmita.ghosh@corteva.com>
2023-05-09 07:28:37 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,15 @@
+import sys
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error Ocuured in Python script name [{0}] line muber [{1}] error message [{2}]".format()
+    file_name,exc_tb.tb_lineno,str(error)
+    return error_message
+    
+class CustomException(Excption):
+   def __init__(self,error_message,error_detail:sys):
+      super.__init__(error_message)
+      self.error_message=error_message_detail(error_message,error_detail=error_detail)
+   def __str__(self):
+      return self.error_message
+
